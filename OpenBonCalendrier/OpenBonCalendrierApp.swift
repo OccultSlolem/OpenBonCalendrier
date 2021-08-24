23cd57e66df60312972a83e1c7c3b64b4512e25a
Ethan Hanlon <ehanlon19@gmail.com>
2021-08-23 20:32:10 -0700

    Set up project

---
+++ OpenBonCalendrier/OpenBonCalendrierApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenBonCalendrierApp.swift
+//  OpenBonCalendrier
+//
+//  Created by Ethan Hanlon on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct OpenBonCalendrierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
